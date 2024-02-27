159a259e7fdc719ee3c876c2de81ca9010916cdc
Jose Molina <khanium@hotmail.com>
2024-02-27 17:52:31 +0100

    added prometheus and grafana dashboards for monitoring

--- cleanup-data.sh
+++ cleanup-data.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "deleting couchbase data..."
 rm -Rf db/couchbase/*
+echo "deleting sync gateway logs..."
+rm -Rf sgw/logs/*
+echo "deleting prometheus data..."
+rm -Rf prometheus/data/*
+echo "deleting grafana db data..."
+rm -Rf grafana/data/*
 echo "clean up completed successfully"
+
