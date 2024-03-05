a424ee30b9455918438ea98ad38f5b7e8cfd7d9c
Jose Molina <khanium@hotmail.com>
2024-03-05 12:28:42 +0100

    updated configs

--- cleanup-data.sh
+++ cleanup-data.sh
@@ -5,6 +5,8 @@ echo "deleting sync gateway logs..."
 rm -Rf sgw/logs/*
 echo "deleting prometheus data..."
 rm -Rf prometheus/data/*
+echo "deleting alertmanager data..."
+rm -Rf alertmanager/data/*
 echo "deleting grafana db data..."
 rm -Rf grafana/data/*
 echo "clean up completed successfully"
