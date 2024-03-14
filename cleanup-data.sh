7d35dbfa25fed30b4418012e98e830e08e2173c5
Jose Molina <khanium@hotmail.com>
2024-03-14 22:53:56 +0100

    added loki and promtail base

--- cleanup-data.sh
+++ cleanup-data.sh
@@ -3,6 +3,8 @@ echo "deleting couchbase data..."
 rm -Rf db/couchbase/*
 echo "deleting sync gateway logs..."
 rm -Rf sgw/logs/*
+echo "deleting promtail log..."
+rm -Rf promtail/log/*
 echo "deleting prometheus data..."
 rm -Rf prometheus/data/*
 echo "deleting alertmanager data..."
